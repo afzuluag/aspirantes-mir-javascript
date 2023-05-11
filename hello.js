691fd8b867f2c6f1fa6d5cb4f164f60816b2e373
Andres Zuluaga <afzuluag@gmail.com>
2023-05-10 19:11:35 -0500

    solucion a los ejercicios de js

---
+++ hello.js
@@ -0,0 +1,7 @@
+function hola(cadenaT){
+    return "Hola " + cadenaT + " !"
+}
+
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
