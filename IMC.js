26f92d8c0415e1ab7bd16fce4e5314057b1722f9
Andres Zuluaga <afzuluag@gmail.com>
2023-05-14 22:55:37 -0500

    Actualizacion datos funcion IMC

--- IMC.js
+++ IMC.js
@@ -5,3 +5,4 @@ function bmi(pPeso, pAltura){
 console.log(bmi(65, 1.8)) // 20.061728395061728
 console.log(bmi(72, 1.6)) // 28.124999999999993
 console.log(bmi(52, 1.75)) //  16.979591836734695
+console.log(bmi(75, 1.69))
